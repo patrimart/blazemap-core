884fc9724d4396e1dbbf1f0389d89a00aa96fcf6
Patrick Martin <bypatrickmartin@gmail.com>
2020-12-02 22:44:02 -0800

    add type to colors

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,32 +1,34 @@
-export const colorsHot = {
+import { ColorGradient } from './types';
+
+export const colorsHot: ColorGradient = {
   0: 0x0000cc00,
   0.4: 0x66009922,
   0.75: 0xff000066,
   1: 0xff8800cc,
 };
 
-export const colorsWarm = {
+export const colorsWarm: ColorGradient = {
   0: 0x0000cc00,
   0.2: 0x0000cc22,
   0.65: 0x88880066,
   1: 0xff0000cc,
 };
 
-export const colorsCold = {
+export const colorsCold: ColorGradient = {
   0: 0x0000cc00,
   0.4: 0x66009922,
   0.75: 0x0066ff66,
   1: 0xeeeeffcc,
 };
 
-export const colorsGrey = {
+export const colorsGrey: ColorGradient = {
   0: 0x33333300,
   0.4: 0x66666622,
   0.75: 0x99999966,
   1: 0xddddddcc,
 };
 
-export const colorsWhite = {
+export const colorsWhite: ColorGradient = {
   0: 0xffffff00,
   0.6: 0xffffff66,
   1: 0xffffffcc,
