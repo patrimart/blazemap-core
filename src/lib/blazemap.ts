ae66c0a599cde8b57666152fc34ae4bfeffe62a8
Patrick Martin <bypatrickmartin@gmail.com>
2021-01-14 00:17:30 -0800

    simplify findMaxCluster

--- src/lib/blazemap.ts
+++ src/lib/blazemap.ts
@@ -59,22 +59,16 @@ export const blazemap = (
   createKernel();
 
   const findMaxCluster = () => {
-    // console.time('findMaxCluster');
-    if (pts.length === 0) {
-      maxWeight = 1;
-      // console.timeEnd('findMaxCluster');
-      return;
-    }
     const diam = (opts.radius - opts.blur * 0.5) * 1.5;
-    const grid = [0];
+    const grid = [1];
+
     for (let i = 0; i < pts.length; i++) {
       const [x, y, p] = pts[i];
       const index = Math.round((x / diam) * (y / diam));
       grid[index] = (grid[index] ?? 0) + p;
     }
+
     maxWeight = grid.reduce((max, v) => Math.max(max, v));
-    // console.timeEnd('findMaxCluster');
-    // console.log({ maxWeight, grid });
   };
 
   /****************************************************************************
